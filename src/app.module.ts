499d9913ddaff0b2d617df7770e712df86b741c8
lmFreire <l.matheus.freire10@gmail.com>
2024-11-23 19:51:11 -0300

    criação de dabatase provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PeopleModule } from './people/people.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [PeopleModule],
+  imports: [PeopleModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
