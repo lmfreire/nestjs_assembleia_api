f545b4f496d32a29c1d7636ebedc020ad1d27f24
lmFreire <l.matheus.freire10@gmail.com>
2024-11-25 21:01:31 -0300

    criacao de entidades de associados e voto

--- src/voto/associado/associado.entity.ts
+++ src/voto/associado/associado.entity.ts
@@ -1,3 +1,11 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class Associado{
     
+    @PrimaryGeneratedColumn()
+    id?: string;
+
+    @Column()
+    cpf: string;
 }
--- src/voto/voto.entity.ts
+++ src/voto/voto.entity.ts
@@ -1,3 +1,26 @@
+import { Pauta } from "src/pautas/pauta.entity";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Associado } from "./associado/associado.entity";
+
+@Entity('voto')
 export class Voto{
-    
+
+    @PrimaryGeneratedColumn()
+    id?: String;
+
+    @ManyToOne(() => Pauta)
+    @JoinColumn({name: 'id_pauta'})
+    pauta: Pauta;
+
+    @ManyToOne(() => Associado)
+    @JoinColumn({name: 'id_associado'})
+    associado: Associado;
+
+    @Column({name: 'voto'})
+    opcaoVoto: OpcaoVoto
+}
+
+export enum OpcaoVoto {
+    SIM = 'SIM',
+    NAO = 'NAO'
 }
