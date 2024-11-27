c745745ece776c3267e4f3461c70495f28d9d93d
lmfreire <l.matheus.freire10@gmail.com>
2024-11-27 12:25:39 -0300

    Adicionando Validações

--- src/voto/voto.resource.ts
+++ src/voto/voto.resource.ts
@@ -1,6 +1,10 @@
+import { IsNotEmpty, IsIn } from "class-validator";
 import { OpcaoVoto } from "./voto.entity";
 
 export class RegistroVotoResource {
+    @IsNotEmpty({message: "Cpf é um campo obrigatório"})
     cpf: string;
+    @IsNotEmpty({message: "Voto é um campo obrigatório"})
+    @IsIn([OpcaoVoto.SIM, OpcaoVoto.NAO], {message: "Campo Opção so aceita valores (SIM,NAO)"})
     voto: OpcaoVoto;
 }
